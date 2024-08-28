62a68e8ded1808f8fa77bf50731aa88e6464bde0
Aung Aung <aaung8@ivytech.edu>
2024-08-27 22:59:49 -0400

    I completed the lecture acitvity

---
+++ Module-2/week_2_lecture_activity.py
@@ -0,0 +1,19 @@
+"""
+create comment with my name
+create string with my name
+create var with the name, name
+have name = name
+print(name)
+print(f"my name is {name}")
+"""
+
+# Aung Aung
+"Aung Aung"
+
+
+name = "Aung Aung"
+print(name)
+
+
+format_string = f"My name is {name}"
+print(format_string)
