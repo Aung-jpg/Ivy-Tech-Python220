3e29a133166e814ae94235cd49897fab5dbfb7e3
Aung Aung <aaung8@ivytech.edu>
2024-09-13 12:21:19 -0400

    code review code with classes about users

---
+++ Module-3/code_review.py
@@ -0,0 +1,25 @@
+class User:
+    def __init__(self, first_name: str, last_name: str, gender: str, favorite_color: str):
+        self.first_name = first_name.title()
+        self.last_name = last_name.title()
+        self.gender = gender
+        self.favorite_color = favorite_color
+
+    def describe_user(self):
+        return f"{self.first_name} {self.last_name} is {self.gender} and likes the color {self.favorite_color}"
+
+people = []
+aung = User("aung", "aung", "male", "red")
+people.append(aung)
+jackson = User("jackson", "ball", "male", "yellow")
+people.append(jackson)
+luis = User("luis", "v-r", "male", "baby blue")
+people.append(luis)
+obeth = User("obeth", "cruze", "male", "green")
+people.append(obeth)
+logan = User("logan", "ghast", "male", "purple")
+people.append(logan)
+
+for i in people:
+    print(i.describe_user())
+
